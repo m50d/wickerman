ceceec47256bc875d5f8a15a690ef3f84ce70ec8
Michael Donaghy <md401@srcf.ucam.org>
2015-10-17 10:50:27 +0100

    this approach seems plausible
    Signed-off-by: Michael Donaghy <md401@srcf.ucam.org>

--- src/main/scala/com/github/m50d/wickerman/FiniteSeq.scala
+++ src/main/scala/com/github/m50d/wickerman/FiniteSeq.scala
@@ -1,6 +1,6 @@
 package com.github.m50d.wickerman
 
-trait FiniteSeq[A] extends SafeFiniteSeq[A] with Seq[A] {
+trait FiniteSeq[A] extends SafeFiniteSeq[FiniteSeq, A] with Seq[A] {
   override def apply(i: Int) = safeApply(i).get
   override def indexWhere(p: A ⇒ Boolean) = safeIndexWhere(p).getOrElse(-1)
   override def reverse: FiniteSeq[A]
--- src/main/scala/com/github/m50d/wickerman/SafeFiniteSeq.scala
+++ src/main/scala/com/github/m50d/wickerman/SafeFiniteSeq.scala
@@ -1,6 +1,6 @@
 package com.github.m50d.wickerman
 
-trait SafeFiniteSeq[A] {
+trait SafeFiniteSeq[C[X] <: SafeFiniteSeq[C, X], A] {
   def safeApply(i: Int): Option[A]
   def safeIndexWhere(p: A ⇒ Boolean): Option[Int]
   def isEmpty: Boolean
@@ -9,8 +9,8 @@ trait SafeFiniteSeq[A] {
   def foldRight[B](z: B)(op: (A, B) ⇒ B): B
   //  def view
   //  def to
-  def map[B](f: A ⇒ B): SafeFiniteSeq[B]
-  def flatMap[B](f: A ⇒ SafeFiniteSeq[B]): SafeFiniteSeq[B]
+  def map[B](f: A ⇒ B): C[B]
+  def flatMap[B](f: A ⇒ C[B]): C[B]
   def ++(other: Iterable[A]): Iterable[A]
   def zip[B](other: Iterable[B]): Iterable[(A, B)]
   def partition(p: A ⇒ Boolean): (Iterable[A], Iterable[A])
